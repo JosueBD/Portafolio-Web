a86aadcd8283b2be57a5135ee4faec4f02a113ac
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-09-26 16:51:22 -0400

    Delete Portafolio-Web/proyectos directory

--- Portafolio-Web/proyectos/contador/js/script.js
+++
@@ -1,5 +0,0 @@
-let count = 0;
-const display = document.getElementById('contador');
-document.getElementById('sum').onclick = () => { display.textContent = ++count; };
-document.getElementById('sub').onclick = () => { display.textContent = --count; };
-document.getElementById('reset').onclick = () => { count = 0; display.textContent = count; };
