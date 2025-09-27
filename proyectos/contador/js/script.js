2724af3a4cbb3d2ae6d03b9776445fef40cde4c3
JosueBD <62859587+JosueBD@users.noreply.github.com>
2025-09-26 22:56:20 -0400

    commit + push.

--- proyectos/contador/js/script.js
+++ proyectos/contador/js/script.js
@@ -1,5 +1,6 @@
 let count = 0;
-const display = document.getElementById('contador');
-document.getElementById('sum').onclick = () => { display.textContent = ++count; };
-document.getElementById('sub').onclick = () => { display.textContent = --count; };
-document.getElementById('reset').onclick = () => { count = 0; display.textContent = count; };
+document.getElementById('btn').addEventListener('click', ()=>{
+  count++;
+  document.getElementById('count').textContent = count;
+});
+
